57a7c4511af5993c8ba70411f2cd0e63082664b4
Hudson McGough <hudsonmcgough@icloud.com>
2025-11-29 23:24:19 -0800

    add api fetch

--- app/index.tsx
+++ app/index.tsx
@@ -5,16 +5,17 @@ export default function Index() {
   
   useEffect(() => {
     // fetch pokemon data here
-
+    fetchPokemonData();
   }, [])
 
   async function fetchPokemonData() {
     try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20");
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20"
+      );
       const data = await response.json();
 
-      console.log(data)
-
+      console.log(data);
     } catch (error) {
       console.log(error);
     }
