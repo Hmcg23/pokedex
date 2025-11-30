18271199524067314e03ab24e20a3132970881dd
Hudson McGough <hudsonmcgough@icloud.com>
2025-11-30 00:42:28 -0800

    add images

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+
+interface Pokemon {
+  name: string;
+  image: string;
+}
 
 export default function Index() {
   // value (pokemons) and function to update it (setPokemons)
-  const [pokemons, setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   
   useEffect(() => {
     // fetch pokemon data here
@@ -17,8 +22,19 @@ export default function Index() {
       );
       const data = await response.json();
 
-      console.log("DATA:", data.results);
-      setPokemons(data.results);
+      // fetch detailed info for each pokemon in parallel
+      const detailedPokemons = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return {
+            name: pokemon.name,
+            image: details.sprites.front_default,
+          }
+        })
+      );
+
+      setPokemons(detailedPokemons);
     } catch (error) {
       console.log(error);
     }
@@ -29,6 +45,10 @@ export default function Index() {
       {pokemons.map((pokemon) => (
         <View key={pokemon.name}>
           <Text>{pokemon.name}</Text>
+          <Image 
+            source={{ uri: pokemon.image }}
+            style={{width: 100, height: 100}}
+          />
         </View>
       ))}
       
